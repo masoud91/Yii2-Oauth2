acfbeeec3aaf53ed95c72364f51dcaa1829cbe0c
Masoud <masoudaghaei.m.a@gmail.com>
2018-07-28 15:59:42 +0430

    mysql grants added

---
+++ grants/mysql/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace infinitydesign\idcoauth\grants;
+
+/**
+ * This is just an example.
+ */
+class Test extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello World.";
+    }
+}
